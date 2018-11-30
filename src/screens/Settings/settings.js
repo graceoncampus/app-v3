545d42d89945aadc4bb98f0744167aa1dfa3f004
Hayley Martinez <hayleydmartinez@gmail.com>
2018-11-29 20:50:51 -0800

    Updated settings

--- src/screens/Settings/settings.js
+++ src/screens/Settings/settings.js
@@ -38,6 +38,7 @@ export default class Settings extends Component {
             loading: true,
             success: false,
             submitted: false,
+            error: ''
         }
         this.unsubscribe = null;
         currentUid = firebase.auth().currentUser.uid;
@@ -68,26 +69,31 @@ export default class Settings extends Component {
 
     onChangeFirstName = (firstName) => { this.setState({ submitted: false, firstName }); }
     onChangeLastName = (lastName) => { this.setState({ submitted: false, lastName }); }
+    onChangePhoneNumber = (phoneNumber) => { this.setState({ submitted: false, phoneNumber }); }
+    onChangeAddress = (address) => { this.setState({ submitted: false, address }); }
+    onChangeGrad = (grad) => { this.setState({ submitted: false, grad }); }
+    onChangeMajor = (major) => { this.setState({ submitted: false, major }); }
+    onChangeHomeChurch = (homeChurch) => { this.setState({ submitted: false, homeChurch }); }
 
     renderButton = () => {
-        if (this.state.loading && !this.state.success) {
+        if (this.state.loading && !this.state.submitted) {
           return (
             <Button style={{ marginBottom: 15, paddingVertical: 15 }}>
                 <Spinner style={{ color: '#fff' }}/>
             </Button>
           );
         }
-    
-        if (this.state.success) {
+
+        if (this.state.submitted) {
           return (
             <Button success style={{ marginBottom: 15, backgroundColor: '#0ab435' }}>
                 <Text style={globalStyles.buttonText} >UPDATED</Text>
             </Button>
           );
         }
-    
+
         return (
-            <Button style={{ marginBottom: 15 }} onPress={() => this.updateInfo}>
+            <Button style={{ marginBottom: 15 }} onPress={this.updateInfo}>
                 <Text style={globalStyles.buttonText} >UPDATE ACCOUNT</Text>
             </Button>
         );
@@ -95,14 +101,31 @@ export default class Settings extends Component {
 
     updateInfo = () => {
         this.setState({ loading: true });
-        const { firstName, lastName, birthday, phoneNumber, address, grad, major, homeChurch, loading } = this.state;
+        const { firstName, lastName, birthday, phoneNumber, address, grad, major, homeChurch, loading, error } = this.state;
         // check if required fields are filled out, show error message if not filled out properly
+        // required: first, last, phone#, grad
+        if (firstName == '' || lastName == '' || phoneNumber == '' || grad == '' ) {
+          this.setState({ error: 'Please fill out all required fields.', loading: false });
+        }
+        else {
+          this.ref.update({
+            firstName,
+            lastName,
+            birthday,
+            phoneNumber,
+            address,
+            grad,
+            major,
+            homeChurch
+          });
+          this.setState({ loading: false, submitted: true });
+        }
 
         // if information filled out correctly, push data to Firebase, update state ( loading: false, submitted: true )
       }
 
     render = () => {
-        const { firstName, lastName, birthday, phoneNumber, address, grad, major, homeChurch, loading } = this.state;
+        const { firstName, lastName, birthday, phoneNumber, address, grad, major, homeChurch, loading, error } = this.state;
 
         if(loading) {
             return (
@@ -119,6 +142,9 @@ export default class Settings extends Component {
                         <Subtitle style={{ color: '#b40a34' }}>* </Subtitle>
                         <Subtitle> is required</Subtitle>
                         </Subtitle>
+                        {(error.length > 0) &&
+                          <Text style={{ color: '#b40a34', paddingBottom: 20, fontWeight: 'bold' }}>{error}</Text>
+                        }
                     </Tile>
                     <FormGroup style={{ paddingHorizontal: 25, flex: 0.56 }}>
                         <View style={{ paddingBottom: 4, flexDirection: 'row'}}>
@@ -126,7 +152,7 @@ export default class Settings extends Component {
                             <Text style={{ color: '#b40a34' }}>* </Text>
                         </View>
                         <TextInput
-                        style={{height: 42, backgroundColor: 'white', borderColor: 'gray', borderWidth: 0.4}}
+                        style={{color: '#202020', paddingLeft: 10, height: 42, backgroundColor: 'white', borderColor: 'gray', borderWidth: 0.4}}
                         autoCapitalize='none'
                         autoCorrect={false}
                         value={firstName}
@@ -138,16 +164,76 @@ export default class Settings extends Component {
                             <Text style={{ color: '#b40a34' }}>* </Text>
                         </View>
                         <TextInput
-                        style={{height: 42, backgroundColor: 'white', borderColor: 'gray', borderWidth: 0.4}}
+                        style={{color: '#202020', paddingLeft: 10, height: 42, backgroundColor: 'white', borderColor: 'gray', borderWidth: 0.4}}
                         autoCapitalize='none'
                         autoCorrect={false}
                         value={lastName}
                         onChangeText={this.onChangeLastName}
                         returnKeyType='next'
                         />
+                        <View style={{ paddingTop: 12, paddingBottom: 4, flexDirection: 'row'}}>
+                            <Text>Phone # </Text>
+                            <Text style={{ color: '#b40a34' }}>* </Text>
+                        </View>
+                        <TextInput
+                        style={{color: '#202020', paddingLeft: 10, height: 42, backgroundColor: 'white', borderColor: 'gray', borderWidth: 0.4}}
+                        autoCapitalize='none'
+                        autoCorrect={false}
+                        value={phoneNumber}
+                        onChangeText={this.onChangePhoneNumber}
+                        returnKeyType='next'
+                        />
+                        <View style={{ paddingTop: 12, paddingBottom: 4, flexDirection: 'row'}}>
+                            <Text>Address </Text>
+                        </View>
+                        <TextInput
+                        style={{color: '#202020', paddingLeft: 10, height: 42, backgroundColor: 'white', borderColor: 'gray', borderWidth: 0.4}}
+                        autoCapitalize='none'
+                        autoCorrect={false}
+                        value={address}
+                        onChangeText={this.onChangeAddress}
+                        returnKeyType='next'
+                        />
+                        <View style={{ paddingTop: 12, paddingBottom: 4, flexDirection: 'row'}}>
+                            <Text>Graduation Year </Text>
+                            <Text style={{ color: '#b40a34' }}>* </Text>
+                        </View>
+                        <TextInput
+                        style={{color: '#202020', paddingLeft: 10, height: 42, backgroundColor: 'white', borderColor: 'gray', borderWidth: 0.4}}
+                        autoCapitalize='none'
+                        autoCorrect={false}
+                        value={grad}
+                        onChangeText={this.onChangeGrad}
+                        returnKeyType='next'
+                        />
+                        <View style={{ paddingTop: 12, paddingBottom: 4, flexDirection: 'row'}}>
+                            <Text>Major </Text>
+                        </View>
+                        <TextInput
+                        style={{color: '#202020', paddingLeft: 10, height: 42, backgroundColor: 'white', borderColor: 'gray', borderWidth: 0.4}}
+                        autoCapitalize='none'
+                        autoCorrect={false}
+                        value={major}
+                        onChangeText={this.onChangeMajor}
+                        returnKeyType='next'
+                        />
+                        <View style={{ paddingTop: 12, paddingBottom: 4, flexDirection: 'row'}}>
+                            <Text>Home Church </Text>
+                        </View>
+                        <TextInput
+                        style={{color: '#202020', paddingLeft: 10, height: 42, backgroundColor: 'white', borderColor: 'gray', borderWidth: 0.4}}
+                        autoCapitalize='none'
+                        autoCorrect={false}
+                        value={homeChurch}
+                        onChangeText={this.onChangeHomeChurch}
+                        returnKeyType='next'
+                        />
                         <Divider />
                         <View style={{ flex: 0.25 }} styleName='vertical h-center v-end'>
                             {this.renderButton()}
+                            <Button style={{ marginBottom: 15 }}>
+                                <Text style={globalStyles.buttonText} >LOG OUT</Text>
+                            </Button>
                         </View>
                     </FormGroup>
                 </KeyboardAwareScrollView>
